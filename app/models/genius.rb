345c459f05ad21bac3d4e547ca24aacf9602119a
Ludvig Londos <ludvig.londos@gmail.com>
2019-06-12 14:01:32 +0200

    added the my pages with my genius

--- app/models/genius.rb
+++ app/models/genius.rb
@@ -1,9 +1,8 @@
 class Genius < ApplicationRecord
-  mount_uploader :photo, PhotoUploader
   validates :name, presence: true
   validates :cost, presence: true
   validates :year, presence: true
   belongs_to :user
   has_many :lectures
-  has_many :photos
+  has_many :photos, dependent: :destroy
 end
